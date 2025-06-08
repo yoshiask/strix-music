09ab4d3683f9d256b4304a08859cdcb0d99ff03b
Yoshi Askharoun <jjask7@gmail.com>
2025-06-08 03:08:11 -0500

    [WIP] Custom accent-based colors in CIELCh

--- src/Shells/StrixMusic.Shells.Media11/AccentColorEffectResourceDictionary.cs
+++ src/Shells/StrixMusic.Shells.Media11/AccentColorEffectResourceDictionary.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Toolkit.Uwp.Helpers;
 using OwlCore.Extensions;
+using SixLabors.ImageSharp.ColorSpaces;
+using SixLabors.ImageSharp.ColorSpaces.Conversion;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Resources;
-using ColorHelperTK = Microsoft.Toolkit.Uwp.Helpers.ColorHelper;
 
 namespace StrixMusic.Shells.Media11;
 
@@ -20,6 +21,7 @@ namespace StrixMusic.Shells.Media11;
 public class AccentColorEffectResourceDictionary : ResourceDictionary
 {
     private readonly UISettings _uiSettings = new();
+    private ColorSpaceConverter _colorConverter = new();
     private Color _accentColor;
 
     /// <inheritdoc/>
@@ -47,10 +49,46 @@ public class AccentColorEffectResourceDictionary : ResourceDictionary
         if (accentColor == _accentColor)
             return;
 
-        var hsvAccentColor = accentColor.ToHsv();
+        Debug.WriteLine($"New accent color {accentColor}, recomputing resources...");
 
-        this["SystemAccentColor_Test"] = ColorHelperTK.FromHsv(hsvAccentColor.H, hsvAccentColor.S * 0.5, hsvAccentColor.V);
+        var rgbBase = new Rgb(accentColor.R / 255f, accentColor.G / 255f, accentColor.B / 255f);
+        //var rgbBase = new Rgb(36 / 255f, 71 / 255f, 178 / 255f);
+        var lchBase = _colorConverter.ToCieLch(rgbBase);
+
+        this["SystemAccentColor"] = ConvertToColor(rgbBase);
+        this["SystemAccentColor_Test"] = ConvertToColor(rgbBase);
+
+        var lchHighlightTarget = _colorConverter.ToCieLch(new Rgb(87 / 255f, 198 / 255f, 239 / 255f));
+        this["SystemAccentColor_HighlightT"] = ConvertToColor(lchHighlightTarget);
+
+        var lchHighlight = new CieLch(
+            LogIncrease(lchBase.L, 1.5f, 100),
+            lchBase.C / 1.75f,
+            HueOffset(lchBase.H, -55)
+        );
+        this["SystemAccentColor_Highlight"] = ConvertToColor(lchHighlight);
 
         _accentColor = accentColor;
+    }
+
+    private Color ConvertToColor(CieLch lch) => ConvertToColor(_colorConverter.ToRgb(lch));
+
+    private static Color ConvertToColor(Rgb rgb) => Color.FromArgb(255, (byte)(rgb.R * 255), (byte)(rgb.G * 255), (byte)(rgb.B * 255));
+
+    private static float LogIncrease(float x, float a, float m)
+    {
+        var head = m - x;
+        return x + (head / a);
+    }
+
+    private static float HueOffset(float hue, float offset)
+    {
+        var h2 = hue + offset;
+        return h2 switch
+        {
+            > 360 => h2 - 360,
+            < 0 => h2 + 360,
+            _ => h2
+        };
     }
 }
